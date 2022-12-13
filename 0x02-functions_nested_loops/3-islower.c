e30ea67d17956daa3013511971dab49c03c1989a
zelalemcheru <zelalemcheru@gmail.com>
2022-12-13 01:30:09 -0800

    islowerletter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower if c is lower case return 1
+ * otherwise return 0
+ * main - check the code.
+ * 
+ * Return: 0 if success ful islower -entry point
+ */
+
+int _islower(int c);
+{
+	if (c >= 97; && c <= 122)
+		return (1);
+	else
+		return (0);
+}
