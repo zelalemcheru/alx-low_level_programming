aa183bc6bbd295ab5221af035a07b1d26d919c4a
Zola123 <46236645+zelalemcheru@users.noreply.github.com>
2022-12-14 10:44:06 +0300

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _islower if c is lower case return 1
- * otherwise return 0
- * main - check the code.
- * 
- * Return: 0 if success ful islower -entry point
+ * _islower - checks if a character is lowercase if YES it returns 1
+ * otherwise returns 0 just like islower() function in ctype.h
+ * @c: Is the int value to be compared with the ASCII value
+ * Return: 0 if successful. _islower - Entry point
  */
 
 int _islower(int c)
 {
-	if (c >= 97; && c <= 122)
+	if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
