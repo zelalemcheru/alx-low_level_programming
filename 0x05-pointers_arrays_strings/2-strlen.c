fcf62845e6ac41d7cf687eb8901eb86ab8739fed
Zola123 <46236645+zelalemcheru@users.noreply.github.com>
2022-12-19 12:03:23 +0300

    Update 2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,12 +2,19 @@
 #include "main.h"
 
 /**
- * _strlen - a function to print the length of a string
- * @s: string to be supplied
- * Return: Always 0 (Success)
- */
+* _strlen - a function to print the length of a string
+* @s: string to be supplied
+* Return: Always 0 (Success)
+*/
 
 int _strlen(char *s)
 {
 	int i = 0;
-}	
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
